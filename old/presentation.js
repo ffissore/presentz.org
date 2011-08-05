7c303c10304fcbbb0edebaf1ba06cb1a95c7ba9d
Federico Fissore <federico@fsfe.org>
2011-08-05 23:54:57 +0200

    keep on working

---
+++ old/presentation.js
@@ -0,0 +1,21 @@
+var presentation = {
+  "chapters" : [ {
+    "title" : "Trailer Sintel",
+    "duration" : "52",
+    "video" : {
+      "url" : "http://www.fissore.org/presents/trailer.webm"
+    },
+    "slide" : {
+      "url" : "http://www.fissore.org/presents/03_misspingus6_1280_1024.png"
+    }
+  }, {
+    "title" : "Trailer Big Buck Bunny",
+    "duration" : "32",
+    "video" : {
+      "url" : "http://www.fissore.org/presents/trailer.ogv"
+    },
+    "slide" : {
+      "url" : "http://www.fissore.org/presents/901ee7e778c8ba6a0dcf19b0323d8ed9.jpg"
+    }
+  } ]
+}
