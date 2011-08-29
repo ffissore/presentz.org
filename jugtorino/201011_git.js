5366ee66dbc8ec2ffce2aef4d9a86b9f40e5bada
Federico Fissore <federico@fsfe.org>
2011-08-29 22:59:56 +0200

    git

---
+++ jugtorino/201011_git.js
@@ -0,0 +1,232 @@
+initPresentz( {
+  "title" : "GIT, (a) Gentle InTroduction",
+  "chapters" : [ {
+    "title" : "GIT, (a) Gentle InTroduction",
+    "duration" : 5676.14,
+    "media" : {
+      "video" : {
+        "url" : "http://blip.tv/ffissore/episode-5499796"
+      },
+      "slides" : [ {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806106222558.swf",
+        "time" : 0
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080610818547.swf",
+        "time" : 3.2561
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806114943970.swf",
+        "time" : 55.7927
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080608891675.swf",
+        "time" : 85.7927
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806161879466.swf",
+        "time" : 113.402
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806130749802.swf",
+        "time" : 154.667
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806100276185.swf",
+        "time" : 160
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806087001594.swf",
+        "time" : 210.667
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806171833937.swf",
+        "time" : 254
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806067762254.swf",
+        "time" : 318
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806059979042.swf",
+        "time" : 379.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806079466773.swf",
+        "time" : 482
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806085061241.swf",
+        "time" : 897
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080615963402.swf",
+        "time" : 976.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806118307315.swf",
+        "time" : 1054
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806050106356.swf",
+        "time" : 1063
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806145179924.swf",
+        "time" : 1095.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806165235906.swf",
+        "time" : 1476
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806081139602.swf",
+        "time" : 1527.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806122263230.swf",
+        "time" : 1538
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806083105127.swf",
+        "time" : 1577.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806090883614.swf",
+        "time" : 1605.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080606954970.swf",
+        "time" : 1630.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080610215251.swf",
+        "time" : 1651.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806152282107.swf",
+        "time" : 1678.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080605646737.swf",
+        "time" : 1697
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806175982376.swf",
+        "time" : 1770
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806173215648.swf",
+        "time" : 1839
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806055019633.swf",
+        "time" : 1867.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806169917661.swf",
+        "time" : 1886
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806049096636.swf",
+        "time" : 1937
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806146679114.swf",
+        "time" : 1999.5
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806166914272.swf",
+        "time" : 2072
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080607120578.swf",
+        "time" : 2193.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806065736065.swf",
+        "time" : 2210.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806158584001.swf",
+        "time" : 2288.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806177142663.swf",
+        "time" : 2301.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806150192715.swf",
+        "time" : 2337.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806112289487.swf",
+        "time" : 2372.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806126181690.swf",
+        "time" : 2401.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806128167369.swf",
+        "time" : 2452.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806047635861.swf",
+        "time" : 2530.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806098277411.swf",
+        "time" : 2565.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806057943305.swf",
+        "time" : 2596.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806077536256.swf",
+        "time" : 2615.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806124151021.swf",
+        "time" : 2668.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806148628835.swf",
+        "time" : 2695.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806053689662.swf",
+        "time" : 2761.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806064109200.swf",
+        "time" : 2894.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806061251704.swf",
+        "time" : 2916.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806168002251.swf",
+        "time" : 3088.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806132041728.swf",
+        "time" : 3127.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806075849420.swf",
+        "time" : 3314.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/20101118080611631107.swf",
+        "time" : 3338.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806094551957.swf",
+        "time" : 3376.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806153913517.swf",
+        "time" : 3419.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806120356753.swf",
+        "time" : 3440.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806179451623.swf",
+        "time" : 3640.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806178334840.swf",
+        "time" : 3651.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/2010111808061041554.swf",
+        "time" : 3675.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806178334840.swf",
+        "time" : 3697.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/2010111808061041554.swf",
+        "time" : 3713.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806178334840.swf",
+        "time" : 3721.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/2010111808061041554.swf",
+        "time" : 3737.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806133215987.swf",
+        "time" : 3789.15
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806052399126.swf",
+        "time" : 3874.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806051203410.swf",
+        "time" : 3932.65
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806061251704.swf",
+        "time" : 4279.12
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806174588202.swf",
+        "time" : 4287.62
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806096409870.swf",
+        "time" : 4294.12
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806092666667.swf",
+        "time" : 4386.12
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806163214573.swf",
+        "time" : 4500.12
+      }, {
+        "url" : "http://jugtorino.presentz.org/201011_git/201011180806110222217.swf",
+        "time" : 4599.12
+      } ]
+    }
+  } ]
+})
