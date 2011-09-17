2af65d04488509e558d759ba9127ab336882be6b
Federico Fissore <federico@fsfe.org>
2011-09-18 00:01:58 +0200

    xtext

---
+++ jugtorino/201003_xtext.js
@@ -0,0 +1,57 @@
+initPresentz( {
+		"title" : "Xtext: un framework per la creazione di DSL",
+  "chapters" : [ {
+    "title" : "Xtext: un framework per la creazione di DSL",
+    "duration" : 1468,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/28604604"
+      },
+      "slides" : [ {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/1.swf",
+        "title" : "Xtext: un framework per la creazione di DSL",
+        "time" : 0
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/2.swf",
+        "title" : "Menu",
+        "time" : 13
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/3.swf",
+        "title" : "Very Important DSL",
+        "time" : 31
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/4.swf",
+        "title" : "Dominio / Tecnologia",
+        "time" : 88
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/5.swf",
+        "title" : "No DSL",
+        "time" : 200
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/6.swf",
+        "title" : "Xtext",
+        "time" : 251
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/7.swf",
+        "title" : "Xtext",
+        "time" : 327
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/8.swf",
+        "title" : "Xtext",
+        "time" : 477
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/9.swf",
+        "title" : "Xtext",
+        "time" : 546
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/10.swf",
+        "title" : "Xtext",
+        "time" : 616
+      }, {
+        "url" : "http://jugtorino.presentz.org/201003_xtext/11.swf",
+        "title" : "Insieme di strumenti",
+        "time" : 757
+      } ]
+    }
+  } ]
+})
