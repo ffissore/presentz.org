325603fa947134324d11704c7b857ce75cd8d1de
Federico Fissore <federico@fsfe.org>
2011-09-19 13:33:52 +0200

    more talks

---
+++ jugtorino/201009_drools.js
@@ -0,0 +1,153 @@
+initPresentz( {
+  "title" : "Drools: declarative programming with rules",
+  "chapters" : [ {
+    "title" : "Drools: declarative programming with rules",
+    "duration" : 6572.3,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/29249429"
+      },
+      "slides" : [ {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445036556965.swf",
+        "title" : "Drools",
+        "time" : 0
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444587658600.swf",
+        "title" : "Declarative Programming",
+        "time" : 9
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 161
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 367
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 616
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 643
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 655
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 669
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 1289
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 1295
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 1542
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 1681
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445060263725.swf",
+        "title" : "Production Rule System",
+        "time" : 1762
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445064935927.swf",
+        "title" : "Drools Modules",
+        "time" : 1827
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 2199
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445064935927.swf",
+        "title" : "Drools Modules",
+        "time" : 2208
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445006057519.swf",
+        "title" : "Drools Expert",
+        "time" : 2669
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444592448295.swf",
+        "title" : "Inference Engine",
+        "time" : 2686
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445006057519.swf",
+        "title" : "Drools Expert",
+        "time" : 2718
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445072609240.swf",
+        "title" : "DEMO",
+        "time" : 2896
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445083158534.png",
+        "title" : "DEMO",
+        "time" : 2909
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/20101017144503240798.png",
+        "title" : "DEMO",
+        "time" : 3355
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445069418615.png",
+        "title" : "DEMO",
+        "time" : 3458
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/20101017144503240798.png",
+        "title" : "DEMO",
+        "time" : 3534
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445002477637.png",
+        "title" : "DEMO",
+        "time" : 3717
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445083158534.png",
+        "title" : "DEMO",
+        "time" : 3765
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445076756920.png",
+        "title" : "DEMO",
+        "time" : 4044
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445010441090.swf",
+        "title" : "Example: disambiguation",
+        "time" : 5081
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445026496755.png",
+        "title" : "DEMO",
+        "time" : 5510
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171444598399582.png",
+        "title" : "DEMO",
+        "time" : 5603
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445010441090.swf",
+        "title" : "Example: disambiguation",
+        "time" : 6100
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445080649703.swf",
+        "title" : "Why to use a Rule Engine",
+        "time" : 6493
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445023616127.swf",
+        "title" : "Expert Systems",
+        "time" : 6512
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445087017301.swf",
+        "title" : "References",
+        "time" : 6533
+      }, {
+        "url" : "http://jugtorino.presentz.org/201009_drools/201010171445056763448.swf",
+        "title" : "Drools Books",
+        "time" : 6553
+      } ]
+    }
+  } ]
+});
