a73d4aaa95ce02d3b53bb08b47af107ffc1050cc
Federico Fissore <federico@fsfe.org>
2011-10-10 13:23:32 +0200

    new talk

---
+++ jugtorino/201102_akka.js
@@ -0,0 +1,133 @@
+initPresetz( {
+	"title" : "Akka - Concurrency, Scalability & Fault Tolerance 2.0 with Actors & STM",
+	"chapters" : [ {
+		"title" : "Akka - Concurrency, Scalability & Fault Tolerance 2.0 with Actors & STM",
+		"duration" : 4553.76,
+		"media" : {
+			"video" : {
+				"url" : "http://vimeo.com/30307084"
+			},
+			"slides" : [ {
+				"url" : "http://jugtorino.presentz.org/201102_akka/20110507133718425983.swf",
+				"title" : "Intro",
+				"time" : 0
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337260726092.swf",
+				"title" : "Concurrency, Scalability & Fault-tolerance 2.0 with Actors & STM",
+				"time" : 10.2075
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337190082153.swf",
+				"title" : "Which are the biggest challenges in contemporary software development?",
+				"time" : 15.2358
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337202493945.swf",
+				"title" : "Deal with overloaded systems",
+				"time" : 20
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337307117933.swf",
+				"title" : "Do concurrency effectively",
+				"time" : 25.094
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337158068082.swf",
+				"title" : "Design, develop and deploy applications that scale well",
+				"time" : 30
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/20110507133718180844.swf",
+				"title" : "The native Java concurrency model",
+				"time" : 35.094
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337249014440.swf",
+				"title" : "Actors: a different model of concurrency",
+				"time" : 45.094
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337266558462.swf",
+				"title" : "People are bad at parallel thinking",
+				"time" : 55.094
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337246245710.swf",
+				"title" : "What is Akka?",
+				"time" : 65.283
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337208658415.swf",
+				"title" : "Untyped Actors",
+				"time" : 75.471
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337255952648.swf",
+				"title" : "Sending message to Untyped Actors",
+				"time" : 192.777
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337233444220.swf",
+				"title" : "Typed Actors",
+				"time" : 692
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337192532235.swf",
+				"title" : "Let it crash fault-tolerance through supervisor hierarchies",
+				"time" : 1199
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337127874817.swf",
+				"title" : "You cant avoid failures: shit happens",
+				"time" : 1297
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/20110507133720513382.swf",
+				"title" : "Whats wrong in trying to prevent errors?",
+				"time" : 1313
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337241149136.swf",
+				"title" : "Fail fast & build self-repairing systems",
+				"time" : 1417
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337187781516.swf",
+				"title" : "Supervised actors",
+				"time" : 1434
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337319542349.swf",
+				"title" : "Declarative supervised actors configuration",
+				"time" : 1748
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337310126638.swf",
+				"title" : "Remote actors",
+				"time" : 1857
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337161226102.swf",
+				"title" : "Server-managed",
+				"time" : 2049
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337313208821.swf",
+				"title" : "Software Transactional Memory",
+				"time" : 2079
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337139866478.swf",
+				"title" : "Why we need an STM?",
+				"time" : 2321
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337212403786.swf",
+				"title" : "How does Akka STM work?",
+				"time" : 2427
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337251664621.swf",
+				"title" : "Transactional Datastructures",
+				"time" : 2672
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337230342327.swf",
+				"title" : "Persistent Datastructures",
+				"time" : 2707
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337164518597.swf",
+				"title" : "Coordinating Actors with STM",
+				"time" : 2937
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337130923021.swf",
+				"title" : "Actors + STM = Transactors",
+				"time" : 3015
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337316277460.swf",
+				"title" : "Other Akka goodies",
+				"time" : 3114
+			}, {
+				"url" : "http://jugtorino.presentz.org/201102_akka/201105071337135616730.swf",
+				"title" : "Thanks a lot! Happy Java (or Scala)",
+				"time" : 3307
+			} ]
+		}
+	} ]
+})
