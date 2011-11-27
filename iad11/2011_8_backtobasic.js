86d20952ed5c54f2e575ce4b7bdba86193796a36
Federico Fissore <federico@fsfe.org>
2011-11-27 09:13:06 +0100

    italian agileday 2011

---
+++ iad11/2011_8_backtobasic.js
@@ -0,0 +1,142 @@
+initPresentz( {
+  "title" : "Back to basic",
+  "chapters" : [ {
+    "title" : "Back to basic",
+    "duration" : 3577,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32500100"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#2",
+        "time" : 10
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#3",
+        "time" : 110
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#4",
+        "time" : 130
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#5",
+        "time" : 154
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#6",
+        "time" : 371
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#7",
+        "time" : 386
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#8",
+        "time" : 409
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#9",
+        "time" : 428
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#10",
+        "time" : 451
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#11",
+        "time" : 482
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#12",
+        "time" : 495
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#13",
+        "time" : 510
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#14",
+        "time" : 573
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#15",
+        "time" : 697
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#16",
+        "time" : 730
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#17",
+        "time" : 761
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#18",
+        "time" : 970
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#19",
+        "time" : 1058
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#20",
+        "time" : 1069
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#21",
+        "time" : 1092
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#22",
+        "time" : 1109
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#23",
+        "time" : 1181
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#24",
+        "time" : 1257
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#25",
+        "time" : 1301
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#26",
+        "time" : 1381
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#27",
+        "time" : 1434
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#28",
+        "time" : 1600
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#29",
+        "time" : 1619
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#30",
+        "time" : 1647
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#31",
+        "time" : 1723
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#32",
+        "time" : 1967
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#33",
+        "time" : 2030
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#34",
+        "time" : 2071
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#35",
+        "time" : 2137
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#36",
+        "time" : 2245
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#37",
+        "time" : 2281
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#38",
+        "time" : 2314
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#39",
+        "time" : 2353
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#40",
+        "time" : 2381
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#41",
+        "time" : 2869
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#42",
+        "time" : 3123
+      }, {
+        "url" : "http://www.slideshare.net/backtobasics-111123164050-phpapp02#43",
+        "time" : 3362
+      } ]
+    }
+  } ]
+})
