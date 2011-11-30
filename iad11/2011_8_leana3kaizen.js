e042ab47938667fcb6f3dc491df772cc2896722a
Federico Fissore <federico@fsfe.org>
2011-11-30 14:16:36 +0100

    leana3kaizen

---
+++ iad11/2011_8_leana3kaizen.js
@@ -0,0 +1,190 @@
+initPresentz( {
+  "title" : "Lean, A3 e Kaizen - Andrea Provaglio",
+  "chapters" : [ {
+    "title" : "Lean, A3 e Kaizen - Andrea Provaglio",
+    "duration" : 3577,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32509793"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#2",
+        "time" : 96
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#3",
+        "time" : 154
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#4",
+        "time" : 201
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#5",
+        "time" : 221
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#6",
+        "time" : 233
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#7",
+        "time" : 237
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#8",
+        "time" : 244
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#9",
+        "time" : 284
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#10",
+        "time" : 380
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#11",
+        "time" : 631
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#12",
+        "time" : 657
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#13",
+        "time" : 683
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#14",
+        "time" : 712
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#15",
+        "time" : 739
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#16",
+        "time" : 785
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#17",
+        "time" : 805
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#18",
+        "time" : 867
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#17",
+        "time" : 871
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#18",
+        "time" : 885
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#19",
+        "time" : 906
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#20",
+        "time" : 920
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#21",
+        "time" : 951
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#22",
+        "time" : 957
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#23",
+        "time" : 960
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#24",
+        "time" : 979
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#25",
+        "time" : 990
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#26",
+        "time" : 1062
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#27",
+        "time" : 1074
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#28",
+        "time" : 1126
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#29",
+        "time" : 1144
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#30",
+        "time" : 1278
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#31",
+        "time" : 1365
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#32",
+        "time" : 1366
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#33",
+        "time" : 1416
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#34",
+        "time" : 1422
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#35",
+        "time" : 1503
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#36",
+        "time" : 1508
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#37",
+        "time" : 1573
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#38",
+        "time" : 1583
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#39",
+        "time" : 1585
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#40",
+        "time" : 1593
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#41",
+        "time" : 1595
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#42",
+        "time" : 1627
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#43",
+        "time" : 1634
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#44",
+        "time" : 1656
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#45",
+        "time" : 1696
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#46",
+        "time" : 1735
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#47",
+        "time" : 1763
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#48",
+        "time" : 1769
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#49",
+        "time" : 1776
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#50",
+        "time" : 1850
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#51",
+        "time" : 1894
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#52",
+        "time" : 1923
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#53",
+        "time" : 1938
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#54",
+        "time" : 2055
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#55",
+        "time" : 2177
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#56",
+        "time" : 2518
+      }, {
+        "url" : "http://www.slideshare.net/a3kaizen-110911025558-phpapp02#57",
+        "time" : 2537
+      } ]
+    }
+  } ]
+})
