86922ddde44e89ad773ec359d4c7eb165c2c5f7f
Federico Fissore <federico@fsfe.org>
2011-12-02 13:55:46 +0100

    debitotecnico

---
+++ iad11/2011_8_debitotecnico.js
@@ -0,0 +1,106 @@
+initPresentz( {
+  "title" : "Capire e gestire il debito tecnico - Alan Franzoni",
+  "chapters" : [ {
+    "title" : "Capire e gestire il debito tecnico - Alan Franzoni",
+    "duration" : 3095,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32732016"
+      },
+      "slides" : [ {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_1.swf",
+        "time" : 0
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_2.swf",
+        "time" : 14
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_3.swf",
+        "time" : 54
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_4.swf",
+        "time" : 63
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_5.swf",
+        "time" : 90
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_6.swf",
+        "time" : 150
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_7.swf",
+        "time" : 216
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_8.swf",
+        "time" : 240
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_9.swf",
+        "time" : 434
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_10.swf",
+        "time" : 594
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_11.swf",
+        "time" : 749
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_12.swf",
+        "time" : 959
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_13.swf",
+        "time" : 1173
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_14.swf",
+        "time" : 1311
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_15.swf",
+        "time" : 1322
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_16.swf",
+        "time" : 1346
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_17.swf",
+        "time" : 1565
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_18.swf",
+        "time" : 1817
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_16.swf",
+        "time" : 1938
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_17.swf",
+        "time" : 1942
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_16.swf",
+        "time" : 1956
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_18.swf",
+        "time" : 1969
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_19.swf",
+        "time" : 2029
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_20.swf",
+        "time" : 2047
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_21.swf",
+        "time" : 2124
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_22.swf",
+        "time" : 2297
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_23.swf",
+        "time" : 2306
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_22.swf",
+        "time" : 2329
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_23.swf",
+        "time" : 2337
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_24.swf",
+        "time" : 2358
+      }, {
+        "url" : "http://iad11.presentz.org/swf/debitotecnico/debito_tecnico_25.swf",
+        "time" : 2364
+      } ]
+    }
+  } ]
+})
