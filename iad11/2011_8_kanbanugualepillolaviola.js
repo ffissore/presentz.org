f168344a1e295d77399e8c74637b449b691d289d
iliasbartolini <ibartoli@thoughtworks.com>
2011-12-03 22:47:37 +0000

    kanbanugualepillolaviola

---
+++ iad11/2011_8_kanbanugualepillolaviola.js
@@ -0,0 +1,292 @@
+initPresentz( {
+  "title" : "Kanban = Pillola Viola - Gaetano Mazzanti",
+  "chapters" : [ {
+    "title" : "Kanban = Pillola Viola - Gaetano Mazzanti",
+    "duration" : 2990,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32730433"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#2",
+        "time" : 63
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#3",
+        "time" : 101
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#4",
+        "time" : 122
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#5",
+        "time" : 135
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#6",
+        "time" : 148
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#7",
+        "time" : 172
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#8",
+        "time" : 206
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#9",
+        "time" : 216
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#10",
+        "time" : 241
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#11",
+        "time" : 252
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#12",
+        "time" : 266
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#13",
+        "time" : 289
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#14",
+        "time" : 316
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#15",
+        "time" : 343
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#16",
+        "time" : 392
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#17",
+        "time" : 425
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#18",
+        "time" : 478
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#19",
+        "time" : 521
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#20",
+        "time" : 551
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#21",
+        "time" : 585
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#22",
+        "time" : 661
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#23",
+        "time" : 679
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#24",
+        "time" : 684
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#25",
+        "time" : 689
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#26",
+        "time" : 711
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#27",
+        "time" : 741
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#28",
+        "time" : 838
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#29",
+        "time" : 881
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#30",
+        "time" : 903
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#31",
+        "time" : 930
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#32",
+        "time" : 945
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#33",
+        "time" : 1001
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#34",
+        "time" : 1003
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#35",
+        "time" : 1005
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#36",
+        "time" : 1008
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#37",
+        "time" : 1014
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#38",
+        "time" : 1041
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#39",
+        "time" : 1092
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#40",
+        "time" : 1148
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#41",
+        "time" : 1188
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#42",
+        "time" : 1252
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#43",
+        "time" : 1280
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#44",
+        "time" : 1307
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#45",
+        "time" : 1378
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#46",
+        "time" : 1415
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#47",
+        "time" : 1459
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#48",
+        "time" : 1470
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#49",
+        "time" : 1472
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#50",
+        "time" : 1477
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#51",
+        "time" : 1503
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#52",
+        "time" : 1556
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#53",
+        "time" : 1617
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#54",
+        "time" : 1627
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#55",
+        "time" : 1640
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#56",
+        "time" : 1648
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#57",
+        "time" : 1696
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#58",
+        "time" : 1765
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#59",
+        "time" : 1858
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#60",
+        "time" : 1866
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#61",
+        "time" : 1872
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#62",
+        "time" : 1930
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#63",
+        "time" : 1971
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#64",
+        "time" : 2003
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#65",
+        "time" : 2032
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#66",
+        "time" : 2165
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#67",
+        "time" : 2234
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#68",
+        "time" : 2238
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#69",
+        "time" : 2287
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#70",
+        "time" : 2303
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#71",
+        "time" : 2305
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#72",
+        "time" : 2321
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#73",
+        "time" : 2338
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#74",
+        "time" : 2396
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#75",
+        "time" : 2441
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#76",
+        "time" : 2461
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#77",
+        "time" : 2463
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#78",
+        "time" : 2470
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#79",
+        "time" : 2506
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#80",
+        "time" : 2539
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#81",
+        "time" : 2559
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#82",
+        "time" : 2572
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#83",
+        "time" : 2581
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#84",
+        "time" : 2609
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#85",
+        "time" : 2632
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#86",
+        "time" : 2663
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#87",
+        "time" : 2688
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#88",
+        "time" : 2747
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#89",
+        "time" : 2804
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#90",
+        "time" : 2826
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#91",
+        "time" : 2833
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#92",
+        "time" : 2850
+      }, {
+        "url" : "http://www.slideshare.net/mgaewsj/kviad11d-111119121916-phpapp01#93",
+        "time" : 2885
+      } ]
+    }
+  } ]
+})
