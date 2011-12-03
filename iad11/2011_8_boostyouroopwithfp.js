b823834f81b147a5722e4c5c7ea7fc6da8e191b4
iliasbartolini <ibartoli@thoughtworks.com>
2011-12-03 23:07:46 +0000

    avevo cannato la successione delle slides da un certo punto in poi :)

--- iad11/2011_8_boostyouroopwithfp.js
+++ iad11/2011_8_boostyouroopwithfp.js
@@ -62,185 +62,170 @@ initPresentz( {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#18",
         "time" : 933
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#16",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#19",
         "time" : 939
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#17",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#20",
         "time" : 958
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#16",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#21",
         "time" : 1133
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#18",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#22",
         "time" : 1355
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#19",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#23",
         "time" : 1423
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#20",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#24",
         "time" : 1428
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#21",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#25",
         "time" : 1431
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#22",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#26",
         "time" : 1489
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#23",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#27",
         "time" : 1556
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#22",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#28",
         "time" : 1647
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#23",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#29",
         "time" : 1691
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#24",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#30",
         "time" : 1716
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#25",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#31",
         "time" : 1754
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#26",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#32",
         "time" : 1773
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#27",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#33",
         "time" : 1798
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#28",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#34",
         "time" : 1799
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#29",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#35",
         "time" : 1816
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#30",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#36",
         "time" : 1839
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#31",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#37",
         "time" : 1960
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#32",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#38",
         "time" : 2023
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#33",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#39",
         "time" : 2102
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#34",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#40",
         "time" : 2107
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#35",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#41",
         "time" : 2108
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#36",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#42",
         "time" : 2112
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#37",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#43",
         "time" : 2268
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#38",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#44",
         "time" : 2448
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#39",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#45",
         "time" : 2481
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#40",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#46",
         "time" : 2546
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#41",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#47",
         "time" : 2561
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#42",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#48",
         "time" : 2562
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#43",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#49",
         "time" : 2563
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#44",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#50",
         "time" : 2564
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#45",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#51",
         "time" : 2595
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#46",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#52",
         "time" : 2626
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#47",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#53",
         "time" : 2700
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#48",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#54",
         "time" : 2707
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#49",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#55",
         "time" : 2733
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#50",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#56",
         "time" : 2734
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#51",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#57",
         "time" : 2735
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#52",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#58",
         "time" : 2750
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#53",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#59",
         "time" : 2751
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#54",
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#60",
         "time" : 2752
       }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#55",
-        "time" : 2765
-      }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#56",
-        "time" : 2768
-      }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#57",
-        "time" : 2771
-      }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#58",
-        "time" : 2759
-      }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#59",
-        "time" : 2762
-      }, {
-        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#60",
-        "time" : 2765
-      }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#61",
-        "time" : 2768
+        "time" : 2765
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#62",
-        "time" : 2769
+        "time" : 2768
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#63",
-        "time" : 2770
+        "time" : 2771
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#64",
-        "time" : 2773
+        "time" : 2759
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#65",
-        "time" : 2775
+        "time" : 2762
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#66",
-        "time" : 2782
+        "time" : 2765
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#67",
-        "time" : 2785
+        "time" : 2768
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#68",
-        "time" : 2789
+        "time" : 2769
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#69",
-        "time" : 2790
+        "time" : 2770
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#70",
-        "time" : 2795
+        "time" : 2773
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#71",
-        "time" : 2805
+        "time" : 2775
       }, {
         "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#72",
-        "time" : 2810
+        "time" : 2782
+      }, {
+        "url" : "http://www.slideshare.net/ubertobarbini/boost-your-oop-with-fp-111119125254-phpapp02#73",
+        "time" : 2785
       } ]
     }
   } ]
