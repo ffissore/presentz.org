d5c7bb5a1563a491e9e39d337cbfa406f39b7b7a
Federico Fissore <federico@fsfe.org>
2011-12-06 13:52:39 +0100

    musto

---
+++ iad11/2011_8_unitestsendtoendtests.js
@@ -0,0 +1,166 @@
+initPresentz( {
+  "title" : "Unit Tests VS End to End Tests - Domenico Musto",
+  "chapters" : [ {
+    "title" : "Unit Tests VS End to End Tests - Domenico Musto",
+    "duration" : 2818,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/33150685"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#2",
+        "time" : 4
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#3",
+        "time" : 43
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#4",
+        "time" : 76
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#5",
+        "time" : 97
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#6",
+        "time" : 143
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#7",
+        "time" : 176
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#8",
+        "time" : 189
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#9",
+        "time" : 315
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#10",
+        "time" : 446
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#11",
+        "time" : 484
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#12",
+        "time" : 538
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#13",
+        "time" : 605
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#14",
+        "time" : 669
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#15",
+        "time" : 752
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#16",
+        "time" : 804
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#17",
+        "time" : 820
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#18",
+        "time" : 860
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#19",
+        "time" : 896
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#20",
+        "time" : 953
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#21",
+        "time" : 975
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#22",
+        "time" : 1005
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#23",
+        "time" : 1106
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#24",
+        "time" : 1118
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#25",
+        "time" : 1155
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#26",
+        "time" : 1173
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#27",
+        "time" : 1218
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#28",
+        "time" : 1242
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#29",
+        "time" : 1270
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#30",
+        "time" : 1278
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#31",
+        "time" : 1295
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#32",
+        "time" : 1305
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#33",
+        "time" : 1337
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#34",
+        "time" : 1398
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#35",
+        "time" : 1409
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#36",
+        "time" : 1439
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#37",
+        "time" : 1471
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#38",
+        "time" : 1517
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#39",
+        "time" : 1549
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#40",
+        "time" : 1566
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#41",
+        "time" : 1684
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#42",
+        "time" : 1907
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#43",
+        "time" : 1924
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#44",
+        "time" : 1974
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#45",
+        "time" : 2007
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#46",
+        "time" : 2016
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#47",
+        "time" : 2047
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#48",
+        "time" : 2079
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#49",
+        "time" : 2171
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#50",
+        "time" : 2196
+      }, {
+        "url" : "http://www.slideshare.net/unittestsvsendtoendtests-111121140028-phpapp01#51",
+        "time" : 2211
+      } ]
+    }
+  } ]
+})
