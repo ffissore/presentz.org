4201c3fa1a2d6e7679df0658506f0a1f18e99355
Federico Fissore <federico@fsfe.org>
2011-12-11 17:17:20 +0100

    jugto guava

---
+++ jugtorino/guava.js
@@ -0,0 +1,57 @@
+initPresentz( {
+  "title" : "Google Guava - Roberto Franchini",
+  "chapters" : [ {
+    "title" : "Google Guava - Roberto Franchini",
+    "duration" : 760,
+    "media" : {
+      "video" : {
+        "url" : "/guava.webm"
+      },
+      "slides" : [
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava1.png",
+            "time" : 0
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava2.png",
+            "time" : 17
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava3.png",
+            "time" : 20
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava4.png",
+            "time" : 29
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava5.png",
+            "time" : 131
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava6.png",
+            "time" : 167
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava7.png",
+            "time" : 246
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava8.png",
+            "time" : 423
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava9.png",
+            "time" : 502
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava10.png",
+            "time" : 563
+          },
+          {
+            "url" : "http://jugtorino.presentz.org/201110_guava/guava11.png",
+            "time" : 647
+          } ]
+    }
+  } ]
+});
