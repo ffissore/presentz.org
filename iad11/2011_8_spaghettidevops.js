3d8e6facc770eeddda11abbe37817d7e649d0880
Federico Fissore <federico@fsfe.org>
2011-12-11 15:38:51 +0100

    more iad talks

---
+++ iad11/2011_8_spaghettidevops.js
@@ -0,0 +1,91 @@
+initPresentz( {
+  "title" : "Spaghetti DevOps - Alessandro Franceschi",
+  "chapters" : [ {
+    "title" : "Spaghetti DevOps - Alessandro Franceschi",
+    "duration" : 3917,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/33444155"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#2",
+        "time" : 106
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#3",
+        "time" : 185
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#4",
+        "time" : 305
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#5",
+        "time" : 512
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#6",
+        "time" : 564
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#7",
+        "time" : 611
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#8",
+        "time" : 721
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#9",
+        "time" : 749
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#10",
+        "time" : 903
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#11",
+        "time" : 969
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#12",
+        "time" : 1204
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#13",
+        "time" : 1563
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#14",
+        "time" : 1810
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#15",
+        "time" : 1959
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#16",
+        "time" : 2041
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#17",
+        "time" : 2585
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#18",
+        "time" : 2623
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#19",
+        "time" : 2766
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#20",
+        "time" : 2860
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#21",
+        "time" : 2903
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#22",
+        "time" : 3066
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#23",
+        "time" : 3075
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#24",
+        "time" : 3100
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#25",
+        "time" : 3500
+      }, {
+        "url" : "http://www.slideshare.net/spaghettidevops-111119082349-phpapp01#26",
+        "time" : 3832
+      } ]
+    }
+  } ]
+})
