1ee9ac75597b81c3c94a0183ad79e7b3e7bb72a4
Federico Fissore <federico@fsfe.org>
2011-12-21 13:36:45 +0100

    cirillo

---
+++ iad11/2011_8_softwareevolutionreallyeffective.js
@@ -0,0 +1,172 @@
+initPresentz( {
+  "title" : "Is Software Evolution really Effective? - Francesco Cirillo",
+  "chapters" : [ {
+    "title" : "Is Software Evolution really Effective? - Francesco Cirillo",
+    "duration" : 3597,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32527081"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#2",
+        "time" : 103
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#3",
+        "time" : 175
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#4",
+        "time" : 382
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#5",
+        "time" : 452
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#6",
+        "time" : 506
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#7",
+        "time" : 567
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#8",
+        "time" : 664
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#9",
+        "time" : 697
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#10",
+        "time" : 707
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#11",
+        "time" : 772
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#12",
+        "time" : 792
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#13",
+        "time" : 1030
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#14",
+        "time" : 1090
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#15",
+        "time" : 1278
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#16",
+        "time" : 1389
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#17",
+        "time" : 1394
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#18",
+        "time" : 1470
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#19",
+        "time" : 1567
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#20",
+        "time" : 1654
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#21",
+        "time" : 1667
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#22",
+        "time" : 1717
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#23",
+        "time" : 1742
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#24",
+        "time" : 1761
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#25",
+        "time" : 1778
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#26",
+        "time" : 1791
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#27",
+        "time" : 1798
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#26",
+        "time" : 1802
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#27",
+        "time" : 1828
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#28",
+        "time" : 1873
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#29",
+        "time" : 1891
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#30",
+        "time" : 1925
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#31",
+        "time" : 1966
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#32",
+        "time" : 1979
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#33",
+        "time" : 1999
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#34",
+        "time" : 2019
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#35",
+        "time" : 2095
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#36",
+        "time" : 2151
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#37",
+        "time" : 2163
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#38",
+        "time" : 2219
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#39",
+        "time" : 2295
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#40",
+        "time" : 2303
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#41",
+        "time" : 2420
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#42",
+        "time" : 2468
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#43",
+        "time" : 2527
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#44",
+        "time" : 2566
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#45",
+        "time" : 2572
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#46",
+        "time" : 2656
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#47",
+        "time" : 2747
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#48",
+        "time" : 2924
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#49",
+        "time" : 3017
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#50",
+        "time" : 3105
+      }, {
+        "url" : "http://www.slideshare.net/issoftwareevolutioneffectiveiad2011v1-rev02-111217121307-phpapp02#51",
+        "time" : 3108
+      } ]
+    }
+  } ]
+})
