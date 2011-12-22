1a9521de5e2116063f51550cf09072ca94d9d341
Federico Fissore <federico@fsfe.org>
2011-12-22 13:45:24 +0100

    volevo solo scrivere codice

---
+++ iad11/2011_8_volevosoloscriverecodice.js
@@ -0,0 +1,238 @@
+initPresentz( {
+  "title" : "Volevo solo scrivere codice - Alberto Brandolini",
+  "chapters" : [ {
+    "title" : "Volevo solo scrivere codice - Alberto Brandolini",
+    "duration" : 2442,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/32729419"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#2",
+        "time" : 3
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#3",
+        "time" : 14
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#4",
+        "time" : 16
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#5",
+        "time" : 45
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#6",
+        "time" : 71
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#7",
+        "time" : 84
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#8",
+        "time" : 96
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#9",
+        "time" : 105
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#10",
+        "time" : 109
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#11",
+        "time" : 114
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#12",
+        "time" : 176
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#13",
+        "time" : 191
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#14",
+        "time" : 196
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#15",
+        "time" : 208
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#16",
+        "time" : 214
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#17",
+        "time" : 217
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#18",
+        "time" : 220
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#19",
+        "time" : 237
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#20",
+        "time" : 247
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#21",
+        "time" : 251
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#22",
+        "time" : 264
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#23",
+        "time" : 301
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#24",
+        "time" : 323
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#25",
+        "time" : 337
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#26",
+        "time" : 365
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#27",
+        "time" : 371
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#28",
+        "time" : 384
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#29",
+        "time" : 435
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#30",
+        "time" : 475
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#31",
+        "time" : 502
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#32",
+        "time" : 526
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#33",
+        "time" : 562
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#34",
+        "time" : 564
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#35",
+        "time" : 569
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#36",
+        "time" : 579
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#37",
+        "time" : 589
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#38",
+        "time" : 595
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#37",
+        "time" : 612
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#38",
+        "time" : 667
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#39",
+        "time" : 865
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#40",
+        "time" : 875
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#41",
+        "time" : 919
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#42",
+        "time" : 940
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#43",
+        "time" : 949
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#44",
+        "time" : 1061
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#45",
+        "time" : 1116
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#46",
+        "time" : 1224
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#47",
+        "time" : 1295
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#48",
+        "time" : 1317
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#49",
+        "time" : 1360
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#50",
+        "time" : 1385
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#51",
+        "time" : 1387
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#52",
+        "time" : 1453
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#53",
+        "time" : 1468
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#54",
+        "time" : 1566
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#55",
+        "time" : 1644
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#56",
+        "time" : 1674
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#57",
+        "time" : 1709
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#58",
+        "time" : 1745
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#59",
+        "time" : 1786
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#60",
+        "time" : 1814
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#66",
+        "time" : 1841
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#67",
+        "time" : 1878
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#68",
+        "time" : 1913
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#69",
+        "time" : 1930
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#70",
+        "time" : 1959
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#71",
+        "time" : 1975
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#72",
+        "time" : 1979
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#73",
+        "time" : 1981
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#74",
+        "time" : 1982
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#75",
+        "time" : 2028
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#76",
+        "time" : 2035
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#77",
+        "time" : 2084
+      }, {
+        "url" : "http://www.slideshare.net/volevosoloscriverecodiceiad2011-111221075409-phpapp01#78",
+        "time" : 2147
+      } ]
+    }
+  } ]
+})
