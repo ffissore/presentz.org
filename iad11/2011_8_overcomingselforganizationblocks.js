c54d3468007e703197ae3c12a2602fcd1045ca12
Federico Fissore <federico@fsfe.org>
2011-12-27 12:11:00 +0100

    provaglio editing

--- iad11/2011_8_overcomingselforganizationblocks.js
+++ iad11/2011_8_overcomingselforganizationblocks.js
@@ -2,7 +2,7 @@ initPresentz( {
   "title" : "Overcoming self-organization blocks - Andrea Provaglio",
   "chapters" : [ {
     "title" : "Overcoming self-organization blocks - Andrea Provaglio",
-    "duration" : 3651,
+    "duration" : 3109,
     "media" : {
       "video" : {
         "url" : "http://vimeo.com/32516509"
@@ -12,79 +12,73 @@ initPresentz( {
         "time" : 0
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#2",
-        "time" : 87
+        "time" : 78
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#3",
-        "time" : 159
+        "time" : 150
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#4",
-        "time" : 282
+        "time" : 273
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#5",
-        "time" : 292
+        "time" : 284
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#6",
-        "time" : 393
+        "time" : 384
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#7",
-        "time" : 524
+        "time" : 515
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#8",
-        "time" : 571
+        "time" : 562
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#9",
-        "time" : 673
+        "time" : 664
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#10",
-        "time" : 766
+        "time" : 757
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#11",
-        "time" : 990
+        "time" : 981
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#12",
-        "time" : 1095
+        "time" : 1086
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#13",
-        "time" : 1100
+        "time" : 1092
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#14",
-        "time" : 1237
+        "time" : 1228
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#15",
-        "time" : 1265
+        "time" : 1256
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#16",
-        "time" : 1412
+        "time" : 1403
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#17",
-        "time" : 1524
+        "time" : 1515
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#18",
-        "time" : 2126
+        "time" : 2117
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#19",
-        "time" : 2144
+        "time" : 2135
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#20",
-        "time" : 2575
+        "time" : 2566
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#21",
-        "time" : 2613
-      }, {
-        "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#22",
-        "time" : 2803
-      }, {
-        "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#23",
-        "time" : 3088
+        "time" : 2604
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#24",
-        "time" : 3330
+        "time" : 2789
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#25",
-        "time" : 3544
+        "time" : 3001
       }, {
         "url" : "http://www.slideshare.net/overcomingself-organizationblocks-110401125354-phpapp02#26",
-        "time" : 3637
+        "time" : 3095
       } ]
     }
   } ]
