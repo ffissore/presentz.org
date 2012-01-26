06f02846873a17d83f3d3ef7ce45a1f8d1821fae
Federico Fissore <federico@fsfe.org>
2012-01-26 18:45:44 +0100

    Gabriele Lana

---
+++ iad11/2011_8_laprofessionedellosviluppatore.js
@@ -0,0 +1,106 @@
+initPresentz( {
+  "title" : "La professione dello sviluppatore - Gabriele Lana",
+  "chapters" : [ {
+    "title" : "La professione dello sviluppatore - Gabriele Lana",
+    "duration" : 3581,
+    "media" : {
+      "video" : {
+        "url" : "http://vimeo.com/34981255"
+      },
+      "slides" : [ {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#1",
+        "time" : 0
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#2",
+        "time" : 77
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#3",
+        "time" : 96
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#4",
+        "time" : 121
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#5",
+        "time" : 265
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#6",
+        "time" : 273
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#7",
+        "time" : 278
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#8",
+        "time" : 303
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#9",
+        "time" : 311
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#10",
+        "time" : 316
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#11",
+        "time" : 325
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#12",
+        "time" : 573
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#13",
+        "time" : 584
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#14",
+        "time" : 664
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#15",
+        "time" : 873
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#16",
+        "time" : 1060
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#17",
+        "time" : 1266
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#18",
+        "time" : 1334
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#19",
+        "time" : 1415
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#20",
+        "time" : 1539
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#21",
+        "time" : 1609
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#22",
+        "time" : 1709
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#23",
+        "time" : 1804
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#24",
+        "time" : 1813
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#25",
+        "time" : 1851
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#26",
+        "time" : 1857
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#27",
+        "time" : 1881
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#28",
+        "time" : 1883
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#29",
+        "time" : 2077
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#23",
+        "time" : 2819
+      }, {
+        "url" : "http://www.slideshare.net/craftsman2011-agileday-111121040632-phpapp02#22",
+        "time" : 3167
+      } ]
+    }
+  } ]
+})
