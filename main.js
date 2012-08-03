d83e7fe0507ca2f9bdf6b23e2869f12cd6a53cbb
Federico Fissore <federico@fsfe.org>
2012-08-03 18:17:20 +0200

    huston, we got a memory problem

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 require("coffee-script");
 require("./presentz.coffee");
+
+setTimeout(function() {
+    console.log(process.memoryUsage());
+    global.gc();
+    console.log(process.memoryUsage());
+}, 10000);
