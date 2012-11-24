952cec05df246ee1a8b0f59dbdf4c96091be6402
Federico Fissore <federico@fsfe.org>
2012-11-24 21:14:38 +0100

    avoid forgetting to update presentz.js version in assets.coffee

--- rebuild_presentz_js.sh
+++ rebuild_presentz_js.sh
@@ -2,3 +2,5 @@ cd ../presentz.js
 cake build
 rm ../presentz.org/public/assets/js/presentz*.js
 cp dist/presentz-1.1.4.js ../presentz.org/public/assets/js/
+cd -
+sed -i 's/presentz-[0-9].[0-9].[0-9]/presentz-1.1.4/g' ./assets.coffee
