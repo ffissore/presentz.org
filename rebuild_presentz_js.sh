8f8a77165dc1e7e33921a759d8e8ac28fb918a01
Federico Fissore <federico@fissore.org>
2013-04-09 02:11:53 +0200

    updating presentz.js

--- rebuild_presentz_js.sh
+++ rebuild_presentz_js.sh
@@ -1,6 +1,6 @@
 cd ../presentz.js
 cake build
-rm ../presentz.org/public/assets/js/presentz*.js
-cp dist/presentz-1.3.0.js ../presentz.org/public/assets/js/
+rm -rf ../presentz.org/public/assets/js/presentz*.js
+cp dist/presentz-1.2.2.js ../presentz.org/public/assets/js/
 cd -
-sed -i 's/presentz-[0-9].[0-9].[0-9]/presentz-1.3.0/g' ./assets.coffee
+sed -i 's/presentz-[0-9].[0-9].[0-9]/presentz-1.2.2/g' ./assets.coffee
